4d5c2d5e924dc1c6b0706447dbba5daf06b68ebd
Pranav Shinde <pranavshinde4019@gmail.com>
2024-07-26 16:05:48 +0530

    Completed Digital Clock Project Practiced useState, useEffect hooks

---
+++ Digital-Clock/src/digitalClock.jsx
@@ -0,0 +1,43 @@
+import React , {useState, useEffect} from "react";
+
+function DigitalClock(){
+
+    const [time , setTime] = useState(new Date());
+
+    useEffect(()=>{
+        const clockInterval = setInterval(()=>{
+               setTime(new Date());
+        }, 1000);
+     
+        return ()=> {clearInterval(clockInterval)}
+        
+    },[]);
+
+
+    function timeformat(){
+        let hours = time.getHours();
+        const minutes = time.getMinutes();
+        const seconds = time.getSeconds();
+        const meridium = hours > 12 ? 'PM' : 'AM';
+        hours = hours % 12 || 12
+
+        return `${padZero(hours)}:${padZero(minutes)}:${padZero(seconds)} ${meridium}`;
+
+    }
+
+     function padZero(number){
+        return((number < 10 ? "0" : "") + number ) 
+     }
+
+
+    return(
+    <div className="clock-container">
+        <div className="clock">
+             <span>{timeformat()}</span>
+        </div>
+
+    </div>)
+
+}
+
+export default DigitalClock
